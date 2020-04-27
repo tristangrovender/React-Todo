4356c4dfc4a48caaf82c80c470f8e74a7bbedd4a
Tristan Grovender <therealtristangrovender@gmail.com>
2020-04-27 14:43:42 -0500

    data is dispaying

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
+import { todos } from "./data";
+import TodoList from "./components/TodoList";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            todos: todos
+        };
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <div className="header">
+                    <h1>todos:</h1>
+                </div>
+                <TodoList todos={this.state.todos} />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const todos = [
+    {
+        task: "Grocery",
+        id: 123,
+        completed: false
+    },
+    {
+        task: "Shower",
+        id: 124,
+        completed: false
+    },
+    {
+        task: "Dishes",
+        id: 1235,
+        completed: false
+    },
+    {
+        task: "Laundry",
+        id: 1246,
+        completed: false
+    },
+    {
+        task: "Walk Hank",
+        id: 1237,
+        completed: false
+    },
+    {
+        task: "Full body workout",
+        id: 1248,
+        completed: false
+    }
+];
